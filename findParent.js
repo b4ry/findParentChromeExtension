4768d837aea2b3ec6cc30db45430b5c93a0f90f0
b4ry <barry90@interia.pl>
2021-10-06 19:08:31 +0200

    Initial commit

---
+++ findParent.js
@@ -0,0 +1,20 @@
+const idInput = document.getElementById("child-element-id-input-ext");
+const confirmButton = document.getElementById("confirm-element-id-button-ext");
+
+confirmButton.addEventListener("click", async () => {
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  const elementId = idInput.value;
+
+  chrome.storage.local.set({ elementId });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: myFunction,
+  });
+});
+
+function myFunction() {
+  chrome.storage.local.get("elementId", function(result) {
+    console.log('Value currently is ' + result.elementId);
+  });
+}
