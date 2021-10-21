c77b72c609c66f44f379ec321834965177c93f22
b4ry <barry90@interia.pl>
2021-10-21 17:40:54 +0200

    Add clear() unit test.

--- spec/content-script.spec.js
+++ spec/content-script.spec.js
@@ -1,12 +1,22 @@
+/**
+ * @jest-environment jsdom
+ */
+
 const contentScript = require("../scripts/content-script");
 
-describe("Content script", () => {
+describe("Content script unit tests:", () => {
   test("chrome.runtime.onMessage must have a listener registered", () => {
     expect(chrome.runtime.onMessage.hasListeners()).toBe(true);
   });
-  // test("clear test", () => {
-  //   contentScript.clear();
-  // });
+  test("clear() must remove the css 'first-parent-extension' class", () => {
+    document.body.innerHTML =
+      '<div class="first-parent-extension">' +
+      '</div>';
+
+    contentScript.clear();
+
+    expect(document.getElementsByClassName("first-parent-extension")).toHaveLength(0);
+  });
   // test("findParent test", () => {
   //   contentScript.clear();
   // });
