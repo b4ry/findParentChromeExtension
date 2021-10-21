9c824acfc6234ae30cec949bb11a4756835daea6
b4ry <barry90@interia.pl>
2021-10-21 18:13:56 +0200

    Add findParent() unit tests testing the behaviour of the method when there is a child selector provided but no element retrieved with it. Clear all the mocks at the end of each test to avoid incrementation of mock call times.

--- spec/content-script.spec.js
+++ spec/content-script.spec.js
@@ -5,6 +5,10 @@
 const contentScript = require("../scripts/content-script");
 
 describe("Content script unit tests:", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   test("chrome.runtime.onMessage must have a listener registered", () => {
     expect(chrome.runtime.onMessage.hasListeners()).toBe(true);
   });
@@ -25,16 +29,34 @@ describe("Content script unit tests:", () => {
     contentScript.findParent();
 
     expect(alertMock).toHaveBeenCalledTimes(1);
-    expect(alertMock)
-      .toHaveBeenLastCalledWith("The element's selector has not been provided. Please, provide an element's selector.");
   });
 
-  test("findParent() calls the alert with a message that no child selector was provided when no child selector provided", () => {
+  test(`findParent() calls the alert with a message that no child selector was provided
+    when no child selector provided`, () => {
     const alertMock = jest.spyOn(window, 'alert').mockImplementation();
 
     contentScript.findParent();
 
     expect(alertMock)
-      .toHaveBeenLastCalledWith("The element's selector has not been provided. Please, provide an element's selector.");
+      .toHaveBeenCalledWith("The element's selector has not been provided. Please, provide an element's selector.");
+  });
+
+  test("findParent() calls the alert when no child found with the provided child selector", () => {
+    const alertMock = jest.spyOn(window, 'alert').mockImplementation();
+
+    contentScript.findParent("testSelector");
+
+    expect(alertMock).toHaveBeenCalledTimes(1);
+  });
+
+  test("findParent() calls the alert with a message that no element could be found with the provided child selector",
+    () => {
+    const alertMock = jest.spyOn(window, 'alert').mockImplementation();
+    const childSelector = "testSelector";
+
+    contentScript.findParent(childSelector);
+
+    const alertMessage = `Could not find element by provided selector: ${childSelector}`;
+    expect(alertMock).toHaveBeenCalledWith(alertMessage);
   });
 });
