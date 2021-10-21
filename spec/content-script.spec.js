bb28a027b91d15c22feab2657130827782dee74a
b4ry <barry90@interia.pl>
2021-10-21 17:52:35 +0200

    Add findParent() unit tests testing the method behaviour when no child selector was provided.

--- spec/content-script.spec.js
+++ spec/content-script.spec.js
@@ -8,6 +8,7 @@ describe("Content script unit tests:", () => {
   test("chrome.runtime.onMessage must have a listener registered", () => {
     expect(chrome.runtime.onMessage.hasListeners()).toBe(true);
   });
+
   test("clear() must remove the css 'first-parent-extension' class", () => {
     document.body.innerHTML =
       '<div class="first-parent-extension">' +
@@ -17,7 +18,23 @@ describe("Content script unit tests:", () => {
 
     expect(document.getElementsByClassName("first-parent-extension")).toHaveLength(0);
   });
-  // test("findParent test", () => {
-  //   contentScript.clear();
-  // });
+
+  test("findParent() calls the alert when no child selector provided", () => {
+    const alertMock = jest.spyOn(window, 'alert').mockImplementation();
+
+    contentScript.findParent();
+
+    expect(alertMock).toHaveBeenCalledTimes(1);
+    expect(alertMock)
+      .toHaveBeenLastCalledWith("The element's selector has not been provided. Please, provide an element's selector.");
+  });
+
+  test("findParent() calls the alert with a message that no child selector was provided when no child selector provided", () => {
+    const alertMock = jest.spyOn(window, 'alert').mockImplementation();
+
+    contentScript.findParent();
+
+    expect(alertMock)
+      .toHaveBeenLastCalledWith("The element's selector has not been provided. Please, provide an element's selector.");
+  });
 });
